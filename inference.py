4deab6b9e453214ec8cf11bdc3b2dafb0caa05ad
Ahmed Hesham <85734497+AhmedHesham34@users.noreply.github.com>
2022-11-11 00:44:53 +0100

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,36 @@
+import os
+import io
+import torchvision.models as models
+from torchvision import transforms
+from PIL import Image
+import torch
+import torch.nn as nn
+
+
+
+def model_fn(model_dir):
+    model = models.resnet18(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    num_features = model.fc.in_features
+    model.fc = nn.Sequential(
+        nn.Linear(num_features, 5))
+
+    with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
+        model.load_state_dict(torch.load(f))
+    return model
+
+
+def input_fn(request_body, content_type):
+    image = Image.open(io.BytesIO(request_body))
+    
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    transformation = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(), normalize])
+
+    return transformation(image).unsqueeze(0)
